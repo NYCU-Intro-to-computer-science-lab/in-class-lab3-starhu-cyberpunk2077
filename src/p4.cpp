9957a43adc75a44f64770eaecf9a4e709875e25b
quan0715 <quan787887@gmail.com>
2025-11-25 04:38:55 +0800

    GUI 自動提交

--- src/p4.cpp
+++ src/p4.cpp
@@ -2,9 +2,30 @@
 
 #include <iostream>
 using namespace std;
-
+void print(int d){
+    for(int i=0;i < d; i++){
+        cout << "|--";
+    }
+}
+int fib(int n, int d){
+    print(d);
+    cout << "SEARCH " << "fib(" << n << ")" << endl;
+    int res = 0;
+    if(n==1){
+        res = 1;
+    }
+    else if(n==2){
+        res = 1;
+    }
+    else{
+        res = fib(n-1, d+1) + fib(n-2, d+1);
+    }
+    print(d);
+    cout << "GET " << "fib(" << n << ") = " << res << endl;
+    return res;
+}
 int main() {
-    
-    
+    int n; cin >> n;
+    cout << fib(n, 0) << endl;
     return 0;
 }
--- src/p5.cpp
+++ src/p5.cpp
@@ -2,9 +2,26 @@
 
 #include <iostream>
 using namespace std;
+void hanoi(int n, int from, int to, int aux){
+    if(n == 0){
+        cout << "Move disk " << n << " from " << from << " to " << to <<endl;
+        return;
+    }
+    if(aux == 2){
+        hanoi(n-1 , from , aux , to);
+        hanoi(n-1 , aux , to , from);
+    }
+    else{
+        hanoi(n-1 , from , aux , to);
+        cout << "Move disk " << n << " from " << from << " to " << to <<endl;
+        hanoi(n-1 , aux , to , from);
+    }
+    
+   
+}
 
 int main() {
-    
-    
+    int n; cin >>n;
+    hanoi(n , 1 , 3 , 2);
     return 0;
 }
