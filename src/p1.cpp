4720ff8aab8e72416847449abaf914cd1fb06028
starhu-cyberpunk2077 <starpig1116@gmail.com>
2025-11-25 14:11:31 +0800

    p1.cpp

--- src/p1.cpp
+++ src/p1.cpp
@@ -1,10 +1,20 @@
-// 題目 1：高塔建築師 (The Tower Architect) - 20分
-
 #include <iostream>
 using namespace std;
-
+long long factorial(int n){
+    int f = 1;
+    for (int i = 1; i <= n; i++){
+        f *= i;
+    }
+    return f;
+}
+long long sum(int n){
+    int s = 0;
+    s = n*(n+1)/2;
+    return s;
+}
 int main() {
-    
-    
+    int n;
+    cin >> n;
+    cout << factorial(n) << endl << sum(n) << endl;
     return 0;
 }
