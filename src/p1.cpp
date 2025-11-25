abc1b3c39d777bcea3b1ac4a8898c60b78ce9938
starhu-cyberpunk2077 <starpig1116@gmail.com>
2025-11-25 14:33:48 +0800

    p1.cpp

--- src/p1.cpp
+++ src/p1.cpp
@@ -1,17 +1,16 @@
 #include <iostream>
 using namespace std;
+
 long long factorial(int n){
-    int f = 1;
-    for (int i = 1; i <= n; i++){
-        f *= i;
-    }
-    return f;
+    if(n == 0 || n == 1)
+        return 1;
+    return (long long)n * factorial(n - 1);
 }
+
 long long sum(int n){
-    int s = 0;
-    s = n*(n+1)/2;
-    return s;
+    return (long long)n * (n + 1) / 2;
 }
+
 int main() {
     int n;
     cin >> n;
