470dcce2cd9d12750f123c514b182b30e7312e7c
starhu-cyberpunk2077 <starpig1116@gmail.com>
2025-11-25 13:52:47 +0800

    Add GCD function and main logic
    Implement Euclidean algorithm for GCD calculation.

--- src/p2.cpp
+++ src/p2.cpp
@@ -1,10 +1,14 @@
-// 題目 2：完美鋪磚計畫 (The Perfect Tiling Project) - 20分
-
-#include <iostream>
+#include<iostream>
 using namespace std;
 
-int main() {
-    
-    
-    return 0;
+int ma(int a,int b){
+    if(a%b==0) return b;
+    return ma(b,a%b);
+}
+
+int main(){
+    int a,b;
+    cin>>a>>b;
+    cout<<ma(a,b)<<endl;
+return 0;
 }
